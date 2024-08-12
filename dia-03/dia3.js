bafe5abb8ef4b8aa02a119fdb8a5636c5be4f77a
gabriel.depiro9@gmail.com <gabriel.depiro9@gmail.com>
2024-08-12 11:15:47 -0300

    mexendo com variáveis

---
+++ dia-03/dia3.js
@@ -0,0 +1,11 @@
+// var - contexto global(não é muito utilizado)
+// let 
+// const - constante (não pode ter o valor alterado)
+
+// let nome = 'Gabriel'
+// console.log(nome)
+
+let num1 = 1;
+let num2 = 2;
+
+console.log(num1 + num2)
