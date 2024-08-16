fb0f7bdcc6ec1f33b9bd3e929a76a843ef93de72
gabriel.depiro9@gmail.com <gabriel.depiro9@gmail.com>
2024-08-16 12:21:45 -0300

    Desafio01 dia-06

--- dia-06/Desafio01.js
+++ dia-06/Desafio01.js
@@ -1,1 +1,8 @@
 let num = Number(prompt("Digite um número: "))
+
+if (num >= 0){
+    for(let cont = 0; cont <= num; cont++)
+        console.log(cont)
+}else{
+    console.log('Número inválido')
+}
