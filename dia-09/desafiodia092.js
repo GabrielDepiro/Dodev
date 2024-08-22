eaf9d32a67d611cd20e09d912381c2d2e70d4ec5
gabriel.depiro9@gmail.com <gabriel.depiro9@gmail.com>
2024-08-22 13:22:35 -0300

    desafio 2 dia-09

--- dia-09/desafiodia092.js
+++ dia-09/desafiodia092.js
@@ -1,10 +1,31 @@
-let nome 
-
-
-console.log('Nome: ' + nome + '\nIdade: ' + idade + '\nSalário: R$' + salarioAtual)
+let nome = ''
+let idade = 0
+let salarioAtual = 0
+let resp = ' '
+let aumento = 0.015
+let ano = 2024
 
 while (resp != 's'){
-    let nome = prompt('Nome: ')
-let idade = Number(prompt('Idade: '))
-let salarioAtual = Number(prompt('Salário: R$'))
+    nome = prompt('Nome: ')
+    idade = Number(prompt('Idade: '))
+    salarioAtual = Number(prompt('Salário: R$'))
+    console.log('Nome: ' + nome + '\nIdade: ' + idade + '\nSalário: R$' + salarioAtual)
+    resp = prompt('Todas as informações estão corretas: [s/n] ')
 }
+
+for(let c = 0; c <= 10; c++){
+  salarioAtual += salarioAtual * aumento
+  console.log(ano + ' - R$' + salarioAtual.toFixed(2))
+  ano++
+  aumento *= 2
+}
+
+//let aumento = 0.015
+//console.log('Previsão salarial para os próximos 10 anos: ')
+
+//for(let ano = 1; ano <= 10; ano++){
+    //salarioAtual += salarioAtual * aumento
+    //aumento *= 2
+
+    //console.log((2023 + ano) + ' = R$' + salarioAtual.toFixed(2))
+//}
